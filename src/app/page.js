ac2d503b39dce93c06cfeaa9895158ec2285e3cf
ramzndmrr <rmzn.dmr.info@gmail.com>
2023-12-11 18:39:47 +0300

    Dowmload dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import {Button, Grid} from "@mui/material";
+"use client"
+import React, { useEffect, useState } from 'react';
+import SideBar from '@/app/layout/SideBar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function Home() {
-  return (
-      <>
+    const [theme, setTheme] = useState(null);
 
-      </>
-  )
+    useEffect(() => {
+        setTheme(createTheme());
+    }, []); //
+
+    if (!theme) {
+        return null;
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <SideBar />
+        </ThemeProvider>
+    );
 }
